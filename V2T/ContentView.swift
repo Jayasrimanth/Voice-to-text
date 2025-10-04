53c6796dd8219238b529ec10b9d6939717684c68
Jayasrimanth <jayasrimanthv05@gmail.com>
2025-10-04 08:11:35 +0530

    Initial Commit

---
+++ V2T/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  V2T
+//
+//  Created by VADDI on 04/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ V2T/V2TApp.swift
@@ -0,0 +1,17 @@
+//
+//  V2TApp.swift
+//  V2T
+//
+//  Created by VADDI on 04/10/25.
+//
+
+import SwiftUI
+
+@main
+struct V2TApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
